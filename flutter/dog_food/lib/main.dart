9be88ea78bccbd7db9096c2d63179ede37064786
Slozzyondul <solomonondula@gmail.com>
2025-01-12 18:30:18 +0300

    flutter version of the app

---
+++ flutter/dog_food/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:dog_food/screens/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system,
+      title: 'Dog Food',
+      theme: ThemeData(
+       
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
---
+++ flutter/dog_food/lib/screens/home_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          leading: Builder(
+            builder: (BuildContext context) {
+              return SizedBox(
+                child: ,
+              );
+            },
+          ),
+          backgroundColor: Colors.brown,
+          title: Center(child: const Text('Home Page')),
+        ),
+      ),
+    );
+  }
+}
